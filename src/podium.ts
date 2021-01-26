6cba37a7a6b71dd62efa48e8ea9cfe3e8b06d0f1
Renato Hormazabal <renato.hormazabal@adevinta.com>
2021-01-26 11:37:50 -0300

    main: fixed linted

--- src/podium.ts
+++ src/podium.ts
@@ -19,24 +19,24 @@ const podium: Module<Options> = function (options) {
    */
   this.nuxt.hook('build:before', (nuxt: any) => {
     nuxt.options.appTemplatePath = path.resolve(__dirname, 'podium.template.html');
-    const skip = (asset) => ({
+    const skip = asset => ({
       ...asset,
-      skip: true,
+      skip: true
     });
     if (nuxt.options.head) {
       const js = nuxt.options.head.script || [];
-      podlet.js(js.map((asset) => ({
+      podlet.js(js.map(asset => ({
         value: asset.src,
         type: asset.type || 'default',
-        nomodule: asset.nomodule,
+        nomodule: asset.nomodule
       })));
-      nuxt.options.head.script = js.map(skip)
+      nuxt.options.head.script = js.map(skip);
       const css = nuxt.options.head.link || [];
-      podlet.css(css.map((asset) => ({
+      podlet.css(css.map(asset => ({
         value: asset.href,
-        rel: asset.rel || 'stylesheet',
+        rel: asset.rel || 'stylesheet'
       })));
-      nuxt.options.head.link = css.map(skip)
+      nuxt.options.head.link = css.map(skip);
     }
   });
 
