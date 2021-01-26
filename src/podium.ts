be801f174406af21c18a1632ba049c4279de03f0
Renato Hormazabal <renato.hormazabal@adevinta.com>
2021-01-26 11:35:05 -0300

    main: save assets from configs

--- src/podium.ts
+++ src/podium.ts
@@ -19,6 +19,25 @@ const podium: Module<Options> = function (options) {
    */
   this.nuxt.hook('build:before', (nuxt: any) => {
     nuxt.options.appTemplatePath = path.resolve(__dirname, 'podium.template.html');
+    const skip = (asset) => ({
+      ...asset,
+      skip: true,
+    });
+    if (nuxt.options.head) {
+      const js = nuxt.options.head.script || [];
+      podlet.js(js.map((asset) => ({
+        value: asset.src,
+        type: asset.type || 'default',
+        nomodule: asset.nomodule,
+      })));
+      nuxt.options.head.script = js.map(skip)
+      const css = nuxt.options.head.link || [];
+      podlet.css(css.map((asset) => ({
+        value: asset.href,
+        rel: asset.rel || 'stylesheet',
+      })));
+      nuxt.options.head.link = css.map(skip)
+    }
   });
 
   this.nuxt.hook('render:route', (_url: any, result: any, { res }: any) => {
